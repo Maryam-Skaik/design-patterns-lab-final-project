3382704998062cc9b8bb87579fe08b2efabc16ae
Maryam Skaik <mariamskaik2003@gmail.com>
2026-06-24 19:50:00 +0300

    Add EventType enum for SHMS event types

---
+++ src/observer/EventType.java
@@ -0,0 +1,12 @@
+package observer;
+
+/**
+ * Defines types of events in the SHMS system.
+ */
+public enum EventType {
+
+    BOOKING_CONFIRMED,
+    CHECK_IN_COMPLETED,
+    PAYMENT_PROCESSED,
+    CHECK_OUT_COMPLETED
+}
